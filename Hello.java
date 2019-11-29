0da1b66da95f8388d4e226af325d7976821a0d34
chlwlgh <chlwlgh0502@hansung.ac.kr>
2019-11-29 22:42:22 +0900

    Second commit

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, world from GIT!");
-        }
+       System.out.println("Hello, world from GIT!");
+       System.out.println("Changes after First commit!");    
+       System.out.println("안녕하세요");
+         }
     }
-
